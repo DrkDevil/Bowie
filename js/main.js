e98d895b81268e6d17adec8e6da725ad5f84ce50
Brian Orr <brian@digitalreligion.net>
2017-02-19 12:00:36 -0600

    Clean Up
    Removed duplicate classes in css. Fixed title to fit the project a bit more.  Added proper indentions in js.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function($){
-	if( $('.videoWrapper').length > 0 ) {
+jQuery(document).ready(function ($) {
+	if ($('.videoWrapper').length > 0) {
 		var videoWrapper = $('.videoWrapper'),
 			mq = window.getComputedStyle(document.querySelector('.videoWrapper'), '::after').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
-		if( mq == 'desktop' ) {
-			var	videoUrl = videoWrapper.data('video'),
-				video = $('<video loop><source src="'+videoUrl+'.mp4" type="video/mp4" /><source src="'+videoUrl+'.webm" type="video/webm" /></video>');
+		if (mq == 'desktop') {
+			var videoUrl = videoWrapper.data('video'),
+				video = $('<video loop><source src="' + videoUrl + '.mp4" type="video/mp4" /><source src="' + videoUrl + '.webm" type="video/webm" /></video>');
 			video.appendTo(videoWrapper);
 			video.get(0).play();
 		}
